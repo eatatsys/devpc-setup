064f469444bc43d0f20b269c2b09fa2b6c45167d
chrisb-de <christian.bielefeldt@berlin.de>
2019-03-21 09:56:44 +0100

    added hyper-v installation

--- SystemInstall.ps1
+++ SystemInstall.ps1
@@ -1,35 +1,10 @@
+# enable hyper-v
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# install choco and packages
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n=allowGlobalConfirmation
-choco install `
-googlechrome `
-microsoft-teams `
-git `
-dotnetfx `
-dotnetcore `
-dotnetcore-sdk `
-nodejs `
-conemu `
-vscode `
-docker-desktop `
-kubernetes-cli `
-Minikube `
-visualstudio2017professional `
-office365business `
-teamviewer `
-adobereader `
-resharper `
-microsoftazurestorageexplorer `
-sql-server-management-studio `
-azure-cli `
-7zip `
-git-fork `
-openssh `
-awscli `
-sysinternals `
-greenshot `
-python `
-python2 `
-procexp `
--y
+choco install packages.config -y
 choco feature disable -n=allowGlobalConfirmation
+
 Restart-Computer
